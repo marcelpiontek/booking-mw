6d2541b4ef39f85cc7394ff5027a413638163e11
Marcel Piontek <mail@marcelpiontek.de>
2023-04-17 13:49:41 +0200

    init and save dummy event for testing

--- src/main/java/de/mpi/bookingmw/event/EventService.java
+++ src/main/java/de/mpi/bookingmw/event/EventService.java
@@ -1,14 +1,12 @@
 package de.mpi.bookingmw.event;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Getter
 @Setter
@@ -25,6 +23,24 @@ public class EventService {
 
     public Optional<Event> getEventById(UUID id) {
         return eventRepo.findById(id);
+    }
+
+    @PostConstruct
+    public void initDummyEvent() {
+        Event event = new Event();
+        event.setTitle("INVisionDay 2023");
+        event.setDescription("Der INVisionDay ist eine ganztägige Veranstaltung der Fakultät Informatik für die " +
+                             "Studierenden der Fachrichtungen Informatik, Medieninformatik und Wirtschaftsinformatik. " +
+                             "Ziel des INVisionDay ist, dass die Studierenden – jenseits der Lehrinhalte aus den " +
+                             "Vorlesungen und Übungen – einen Einblick in aktuelle IT-Themen aus der Forschung und der " +
+                             "Unternehmenspraxis erhalten.");
+        event.setDate(new Date());
+        event.setLocation("Technische Hochschule Nürnberg, Keßlerplatz 12, 90489 Nürnberg");
+        event.setInfo("Indiana Codes und der heilige Gral der Softwareentwicklung - Workshop");
+        event.setMaxParticipants(30);
+        event.setReplyAddress("marcel.piontek@quanos.com");
+        event.setTags(Arrays.asList("code", "workshop"));
+        eventRepo.save(event);
     }
 
 }
